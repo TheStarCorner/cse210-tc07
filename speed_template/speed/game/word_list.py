512b00ae2c0421cb2de8c3af62952bdfe7d70637
Ammon Nelson <Ammon1nelson@gmail.com>
2021-06-08 15:59:43 -0600

    This is the Word_list Class

---
+++ speed_template/speed/game/word_list.py
@@ -0,0 +1,46 @@
+from game import word
+
+class WordList:
+    """ stores words to be used on screen
+
+    type: Info holder
+
+    Attributes:
+    self._list_of_words
+    
+    Methods:
+    add_word(self, word)
+        Parameters:
+            word: string
+        Returns:
+            void 
+    does_contain(self, word)
+        Parameters:
+            word: string
+        Returns:
+            boolean 
+    delete_word(self, word)
+        Parameters:
+            word: string
+        Returns:
+            void 
+    """
+    def __init__(self):
+        self._list_of_words = word 
+
+
+    def add_word(self,word):
+        #add word onto word_list
+        self._list_of_words.append(word)
+
+
+    def does_contain(self,word):
+        #checks to see if given word is in the word list.
+        if word == word:
+            return True
+
+        else: return False
+    
+    def delete_word(self,word):
+        #delete word from screen
+        self._list_of_words.remove(word)
