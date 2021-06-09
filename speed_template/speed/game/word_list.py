994af0cdf15d85a08da29cc769c9fb0ef472f847
Ammon Nelson <Ammon1nelson@gmail.com>
2021-06-09 15:10:18 -0600

    More changes to word_list but not yet completed yet

--- speed_template/speed/game/word_list.py
+++ speed_template/speed/game/word_list.py
@@ -26,7 +26,7 @@ class WordList:
             void 
     """
     def __init__(self):
-        self._list_of_words = word 
+        self._list_of_words = [word]
 
 
     def add_word(self):
@@ -35,12 +35,19 @@ class WordList:
 
 
     def does_contain(self,word):
-        #checks to see if given word is in the word list.
-        if word == word:
+        #checks to see if given word is in the word list. 
+
+        # need to have a something along the lines of self._list_of_words.contains(word) checking to see if it contains the
+        # word that we have typed. 
+        if self._list_of_words.contains(word):
             return True
 
         else: return False
     
     def delete_word(self,word):
         #delete word from screen
-        self._list_of_words.remove(word)
+        # need to have a loop that checks the current word to the word that we have in the list currently.
+        if self.word == self._list_of_words.contains(word):
+            self._list_of_words.remove(word)
+        else:
+           # do nothing you idiot computer
