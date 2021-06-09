42c64f81eae9c2dd338e400fa901ece96d84d94e
Manoel <gal18021@byui.edu>
2021-06-09 16:25:14 -0600

    "word updated final"

--- speed_template/speed/game/word.py
+++ speed_template/speed/game/word.py
@@ -1,7 +1,7 @@
+from actor import Actor
+import constants
 import random
-from game.actor import Actor
-from game.point import Point
-from game.word_list import WordList
+
 
 class Word(Actor):
     """Verify if the word typed matches 
@@ -12,30 +12,38 @@ class Word(Actor):
     Attributes:
         _user_word_typed (?) (string): The sequence of letters typed by the user
     """
+
     
-    def __init__(self, word):
+    def __init__(self):
         """The class constructor.
         
         Args:
             self (Word): An instance of Word.
+            word (string)
         
         """
+        
+        i = random.randint(0, len(constants.LIBRARY))
+
+        word = constants.LIBRARY[i]
         self._word = word
+
+        print(self._word)
         
 
-    def equals(self, other):
+    def equals(self, word):
         """Whether this Word is equal to the word randomly chosen.
 
         Args:
             self (Word): An instance of Word.
-            other (Word): The Word to compare.
+            word (Word): The Word to compare.
 
         Returns: 
             boolean: True if both word and randword are equal; false if otherwise.
         """
-        return self._word == other.get_randword()
+        return self._word == word
 
-    def get_randword(self):
+    def get_word(self):
         """Gets the random word.
         
         Args:
@@ -45,7 +53,6 @@ class Word(Actor):
             string: A chosen word.
         """
         
-        return self._randword
+        return self._word
 
-
- 
+a = Word()
